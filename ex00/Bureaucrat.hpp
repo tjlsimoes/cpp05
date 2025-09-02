ba1b2ffe9ec8806132637b613f569c8e6d9fd001
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-09-02 15:03:59 +0100

    Sketch solution for ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <string>
+#include <iostream>
+class	Bureaucrat
+{
+	private:
+		const std::string 	_name;
+		int					_grade;
+	
+	public:
+		Bureaucrat();										// Default Constructor
+		Bureaucrat(const std::string n, int g);				// Parameterized Constructor
+		Bureaucrat(const Bureaucrat & other);				// Copy Constructor
+		Bureaucrat	&operator=(const Bureaucrat &other);	// Assignment Operator
+		~Bureaucrat();										// Destructor
+
+		class GradeTooHighException : public std::exception 
+		{
+			public:
+				const char	*what() const noexcept override
+				{
+					return ("Invalid grade: too high");
+				}
+		};
+		class GradeTooLowException : public std::exception 
+		{
+			public:
+				const char	*what() const noexcept override
+				{
+					return ("Invalid grade: too low");
+				}
+		};
+
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrementGrade();
+};
+
+std::ostream	&operator<<(std::ostream & o, const Bureaucrat & rhs);
+
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,68 @@
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	Bureaucrat def;
+	std::cout << def;
+
+	Bureaucrat a("Neo", 1);
+	Bureaucrat b("Smith", 150);
+	std::cout << a << b;
+
+	std::cout << "-------- Object construction exceptions --------" << std::endl;
+	// Exceptions upon object construction
+	try
+	{
+		Bureaucrat c("Zed", 0);
+	}
+	catch(const Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat d("Mr. Williams", 151);
+	}
+	catch(const Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "-------- Increment and decrement tests --------" << std::endl;
+	// Increment and decrement tests
+	def.decrementGrade();
+	a.decrementGrade();
+	b.incrementGrade();
+	std::cout << def << a << b;
+	try
+	{
+		def.incrementGrade();
+		def.incrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		b.decrementGrade();
+		b.decrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Bureaucrat *e = new Bureaucrat("James", 2);
+	e->incrementGrade();
+		try
+	{
+			e->incrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	delete e;
+	
+}
