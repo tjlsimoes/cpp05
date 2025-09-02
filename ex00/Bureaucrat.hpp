8033983af1f5d66ab5f199969c4f6fdce9de130f
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-09-02 16:11:59 +0100

    Fix c++ compilation flags

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -18,7 +18,7 @@ class	Bureaucrat
 		class GradeTooHighException : public std::exception 
 		{
 			public:
-				const char	*what() const noexcept override
+				const char	*what()
 				{
 					return ("Invalid grade: too high");
 				}
@@ -26,7 +26,7 @@ class	Bureaucrat
 		class GradeTooLowException : public std::exception 
 		{
 			public:
-				const char	*what() const noexcept override
+				const char	*what()
 				{
 					return ("Invalid grade: too low");
 				}
